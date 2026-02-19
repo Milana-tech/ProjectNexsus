9fc41b7bff8e42397bd46d872e33b887c840d1cb
Polina <91316935+Polinalavender@users.noreply.github.com>
2026-02-19 17:20:55 +0100

    Add dashboard files

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GreenhouseDashboard from './dashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<GreenhouseDashboard />);
